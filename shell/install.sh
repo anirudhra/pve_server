77bd09d350311299884e01423c92a7463b9a484f
Anirudh Acharya <anirudhra@users.noreply.github.com>
2024-08-11 18:19:30 -0700

    Update install.sh

--- shell/install.sh
+++ shell/install.sh
@@ -1,6 +1,10 @@
 apt update
 apt upgrade
-apt install vim btop htop duf avahi-daemon avahi-utils alsa-utils cpufrequtils lm-sensors usbutils pciutils
+apt install vim btop htop duf avahi-daemon avahi-utils alsa-utils cpufrequtils lm-sensors usbutils pciutils intel-gpu-tools
+# foss intel driver - install one of below
+# apt install intel-media-va-driver
+# non-free intel driver
+apt install intel-media-va-driver-non-free
 apt clean
 apt autoclean
 apt autoremove
