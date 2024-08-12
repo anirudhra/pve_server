93ad72e0d3ed8929f474c68c39d9c2db49df1bc5
Anirudh Acharya <anirudhra@users.noreply.github.com>
2024-08-11 17:13:10 -0700

    Add files via upload

---
+++ pve_scripts/scripts/hdpool_zfs_mount.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# script to mount usb zfs pool
+
+# uncomment -f version if pool fails to mount, else use non-f version
+#zpool import hdpool -f
+zpool import hdpool
+
+# following command may not be needed all the time, but just in case
+zfs set mountpoint=/hdpool hdpool
+
