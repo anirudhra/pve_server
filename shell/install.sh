e6136cce04712c45af28a20ff1fac3545992974e
Anirudh Acharya <anirudhra@users.noreply.github.com>
2024-08-17 10:05:42 -0700

    Update install.sh

--- shell/install.sh
+++ shell/install.sh
@@ -6,13 +6,16 @@ apt upgrade
 # apt install vim btop htop duf avahi-daemon avahi-utils alsa-utils cpufrequtils lm-sensors drivetemp vainfo usbutils pciutils intel-gpu-tools autofs
 #
 # following section is for PVE LXC
-apt install vim btop htop duf avahi-daemon avahi-utils alsa-utils autofs
+apt install vim btop htop duf avahi-daemon avahi-utils autofs
+#
+# following section is if you have audio passthrough in LXC
+# apt install alsa-utils
 #
 # following section is if you have iGPU passthrough in LXC
 # foss intel driver - install one of below (only decode)
 # apt install intel-media-va-driver vainfo intel-gpu-tools
 # non-free intel driver (both decode and encode)
-apt install intel-media-va-driver-non-free vainfo intel-gpu-tools
+# apt install intel-media-va-driver-non-free vainfo intel-gpu-tools
 #
 # on kodi hosts, install the following
 # apt install kodi-inputstream-adaptive
@@ -25,4 +28,6 @@ wget https://raw.githubusercontent.com/anirudhra/hpe800g4dm_server/main/shell/do
 # source aliases in .profile after creating backup
 cp ~/.profile ~/.profile.bak
 echo "source ~/.aliases" >> ~/.profile
+echo
+echo "Done! Logout and log back in for changes"
 
