283b8f47f7873461a5170e5d2106f2192ecb5b9b
Anirudh Acharya <anirudhra@users.noreply.github.com>
2024-08-22 17:28:53 -0700

    Create rsync_backup_clone.sh

---
+++ pve_lxc_scripts/maintenance/rsync_backup_clone.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+# script to use rsync for backups and cloning disks
+# syntax: script.sh source/ destination/
+# replicates all files inside source directory in destination directory. leading slash is required
+rsync -avxHAXW --progress $1 $2
