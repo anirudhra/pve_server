aa7d5ca8ebdfdea39a651c3616c407c3451e126e
root <root@pve.acharya>
2024-12-29 15:23:21 -0800

    Adding new scripts

---
+++ pve_lxc_scripts/maintenance/ipodsync.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# (c) Anirudh Acharya 2024
+# iPod Music and Playlists Sync script
+# Run this script from PVE Server (not one of the LXCs or VMs)
+# Only one iPod must be connected when the script is invoked
+# Script will keep iPod in sync with the music and playlists on the Server/NAS
+# This script expects certain paths/names/hostnames etc. to work correctly
+
+
