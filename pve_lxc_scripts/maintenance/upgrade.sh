3655f70da5047a574d61193fc5adeea66b916b45
anirudhra <anirudhra@hotmail.com>
2025-06-07 10:48:42 -0700

    Adding upgrade script

---
+++ pve_lxc_scripts/maintenance/upgrade.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+fullclean() 
+{
+  sudo apt clean
+  sudo apt autoclean
+  sudo apt autoremove
+}
+
+fullupdate()
+{
+  sudo apt update
+  sudo apt dist-upgrade
+  
+  #pve server only
+  if [ -x "$(command -v pveversion)" ]; then
+    sudo fwupdate
+  fi  
+}
+
+#intended to be run on PVE guests only
+dockerupdateall()
+{
+    if [ ! -x "$(command -v pveversion)" ]; then #ensure it's not the server 
+      cd /mnt/pve-sata-ssd/ssd-data/dockerapps || exit
+      cd "$(hostname)" || exit
+      find . -maxdepth 1 -type d \( ! -name . \) -not -path '*disabled*' -exec bash -c "cd '{}' && pwd && docker compose down && docker compose pull && docker compose up -d --remove-orphans" \;
+      docker image prune -a -f
+      docker system prune --volumes -f
+    fi
+}
+
+#main
+fullupdate
+fullclean
+dockerupdateall
+
